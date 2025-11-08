23eb2c9c4d605959e2df2f1c03d1c00368a111ac
Stephen Sulimani <stephensulimani@gmail.com>
2025-11-08 10:11:55 -0500

    :tada: initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import type { ApiResponse } from "shared/dist";
+
+export const app = new Hono()
+
+  .use(cors())
+
+  .get("/", (c) => {
+    return c.text("Hello Hono!");
+  })
+
+  .get("/hello", async (c) => {
+    const data: ApiResponse = {
+      message: "Hello BHVR!",
+      success: true,
+    };
+
+    return c.json(data, { status: 200 });
+  })
+
+  .get("/healthcheck", async (c) => {
+    const data: ApiResponse = {
+      message: "ok",
+      success: true,
+    };
+
+    return c.json(data, { status: 200 });
+  });
+
+export default app;
+
